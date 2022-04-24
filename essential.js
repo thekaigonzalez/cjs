c1868fa5c036024c8433327d228de1f28de101da
Kai Gonzalez <gkai70264@gmail.com>
2022-04-24 13:09:09 -0400

    Add new help function.

---
+++ essential.js
@@ -0,0 +1,8 @@
+// Licensed under the GNU GPL v3 License
+
+include("stdio")
+
+function help() {
+    printf("Welcome to CJS!")
+    printf("CJS (Also called IronJS) is a JavaScript runtime with a tiny standard library inspired by C's standard library.")
+}
